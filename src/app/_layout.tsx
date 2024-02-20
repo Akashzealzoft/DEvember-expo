c0f34e636ef14bcf107d36dfbdd285afc72bdf8d
ariefzealzoft <arief@zealzoft.com>
2024-02-20 20:04:39 +0530

    load fonts in layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router"
+import { useEffect } from "react"
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { NanumPenScript_400Regular } from '@expo-google-fonts/nanum-pen-script';
+import * as SplashScreen from 'expo-splash-screen';
+
 
 export default function RootLayout(){
+    const [fontsLoaded, fontError]=useFonts({
+        Inter:Inter_900Black,
+        PenScript:NanumPenScript_400Regular
+      })
+  
+      
+  
+      useEffect(()=>{
+        if (fontsLoaded || fontError) {
+          SplashScreen.hideAsync()
+         }
+      },[fontsLoaded,fontError])
+  
+  
+  
+      if (!fontsLoaded && !fontError) {
+          return null
+       }
     return <Stack/>
 }
